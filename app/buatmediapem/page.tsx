3dbdf33588b242aef12cbae6d63101b5ba00fcdc
Berkah Lestari Jambi (BLJ) <hironimusharyanto36@guru.sma.belajar.id>
2026-08-30 18:54:51 +0700

    Update page.tsx

--- app/buatmediapem/page.tsx
+++ app/buatmediapem/page.tsx
@@ -5,6 +5,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
+
 // Set worker eksternal menggunakan CDN resmi terverifikasi
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
