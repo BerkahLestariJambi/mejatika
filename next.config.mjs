4f741035e9b236f880f6874a0734f937f5d72882
Berkah Lestari Jambi (BLJ) <hironimusharyanto36@guru.sma.belajar.id>
2026-08-30 20:41:18 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             // PENTING: Menambahkan blob: dan data: di dalam connect-src
-            value: "default-src 'self'; frame-src 'self' https://www.youtube.com https://youtube.com https://drive.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; connect-src 'self' blob: data: https://backend.mejatika.com https://www.youtube.com;",
-          },
+            value: "default-src 'self'; frame-src 'self' https://www.youtube.com https://youtube.com https://drive.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; connect-src 'self' blob: data: https://backend.mejatika.com https://mejatika.com https://www.youtube.com;",          },
         ],
       },
     ]
