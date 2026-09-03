a38ed0b38ddb11c6ac81f994ac74e06b64e82ecf
Berkah Lestari Jambi (BLJ) <hironimusharyanto36@guru.sma.belajar.id>
2026-09-03 19:42:06 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,8 +16,8 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            // Konfigurasi CSP yang melonggarkan akses khusus untuk pemutar YouTube dan Google Drive
-            value: "default-src 'self'; frame-src 'self' https://www.youtube.com https://youtube.com https://drive.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; connect-src 'self' https://backend.mejatika.com https://www.youtube.com;",
+            // DITAMBAHKAN: media-src 'self' blob: data: dan worker-src 'self' blob:
+            value: "default-src 'self'; media-src 'self' blob: data:; worker-src 'self' blob:; frame-src 'self' https://www.youtube.com https://youtube.com https://drive.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; connect-src 'self' https://backend.mejatika.com https://www.youtube.com;",
           },
         ],
       },
